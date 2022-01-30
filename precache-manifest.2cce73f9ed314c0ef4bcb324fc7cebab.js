d00916a8500e9d031d4d6b0e9bbbd25924acae34
kishore-raj <j.kishoreraj93@gmail.com>
2022-01-30 11:43:15 +0530

    Updates

---
+++ precache-manifest.2cce73f9ed314c0ef4bcb324fc7cebab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fefe8b50dda85ea2850eb137ed6c0fa3",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "56f16ecee667da13aae8",
+    "url": "/profile/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f61c544cdea0fd1230b7",
+    "url": "/profile/static/js/2.675def31.chunk.js"
+  },
+  {
+    "revision": "bd81e5c6feaf63e9a711",
+    "url": "/profile/static/js/3.4183067e.chunk.js"
+  },
+  {
+    "revision": "56f16ecee667da13aae8",
+    "url": "/profile/static/js/main.7751c17a.chunk.js"
+  },
+  {
+    "revision": "6e5237cb65ca9dc1c184",
+    "url": "/profile/static/js/runtime~main.5ba45b0b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/profile/static/media/logo.06e73328.svg"
+  }
+]);
